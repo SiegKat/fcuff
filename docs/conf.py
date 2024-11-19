412e072773e5bb8e58d6f05a9dc87de7858073d7
Thiago Costa <122941069+SiegKat@users.noreply.github.com>
2024-11-19 02:12:46 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,13 +33,10 @@ release = '0.0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
