68ec0d08a049ca34a49772e491e4451c6339ca02
Thiago Costa <122941069+SiegKat@users.noreply.github.com>
2024-11-20 10:39:14 -0500

    Update setup.py

--- gui/setup.py
+++ gui/setup.py
@@ -4,12 +4,12 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name = 'H2Insights',
+  name = 'fcuff',
   version = '0.0.1',
   description = 'Análise e processamento de dados de experimentos com célula combústivel e eletrolizadores',
   long_description = long_description,
   long_description_content_type = 'text/markdown',
-  url = 'https://github.com/SiegKat/FuelCellUFF',
+  url = 'https://github.com/SiegKat/fcuff',
   author = 'Thiago Costa',
   author_email = 'tabrantedaco2023@fau.edu',
   license = 'MIT',
@@ -22,7 +22,7 @@ setuptools.setup(
   python_requires='>=3.13',
   install_requires=['numpy', 'pandas', 'matplotlib', 'scipy', 'PySide6', 'PyQT6'],
   project_urls={
-    #'Documentation': '',
-    'Source': 'https://github.com/SiegKat/FuelCellUFF'
+    'Documentation': 'https://siegkat.github.io/fcuff/',
+    'Source': 'https://github.com/SiegKat/fcuff'
   }
 )
