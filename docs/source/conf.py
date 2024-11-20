c4fdc5dc6a4653c0af62b1dd144907185868fc2c
Thiago Costa <122941069+SiegKat@users.noreply.github.com>
2024-11-20 18:08:52 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,23 +9,38 @@
 project = 'fcuff'
 copyright = '2024, Thiago Costa'
 author = 'Thiago Costa'
-release = '0.0.1'
+release = '0.1'
+version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-   'sphinx.ext.duration',
-   'sphinx.ext.doctest',
-   'sphinx.ext.autodoc',
-   'sphinx.ext.autosummary',
-   'sphinx_rtd_theme',
-   'myst_parser',
-]
-autosummary_generate = True
 
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
+    'myst_parser',
+]
+
+autosummary_generate = True
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
-exclude_patterns = []
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+
 
 
 
@@ -33,5 +48,8 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 html_js_files = [("readthedocs.js", {"defer": "defer"})]
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
