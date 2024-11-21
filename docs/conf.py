04607c2a152b59de33e36a8e2d6edf763ba6af39
Thiago Costa <122941069+SiegKat@users.noreply.github.com>
2024-11-21 04:00:13 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ templates_path = ['_templates']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+#import sphinx_rtd_theme
+#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
