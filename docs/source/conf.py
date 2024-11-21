e534eaf40a581452c6f950bffaf1836a7f9633ae
Thiago Costa <122941069+SiegKat@users.noreply.github.com>
2024-11-20 21:30:56 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,9 +8,10 @@
 
 project = 'fcuff'
 copyright = '2024, Thiago Costa'
-author = 'Thiago Costa'
+author = 'Thiago'
+
 release = '0.1'
-version = '0.1.0'
+version = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,21 +29,13 @@ extensions = [
     'myst_parser',
 ]
 
-autosummary_generate = True
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
+
 intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
-
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
